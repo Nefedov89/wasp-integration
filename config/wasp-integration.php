d33ba87ba9f10fe0fed04b31b299c8e46868f674
Sergey Nefedov <nefedovs89@gmail.com>
2019-06-19 18:21:50 +0300

    fixed base api path in config

--- config/wasp-integration.php
+++ config/wasp-integration.php
@@ -3,6 +3,6 @@
 declare(strict_types = 1);
 
 return [
-    'api_base_url'      => 'https://dri.waspinventorycloud.com/public-api',
+    'api_base_url'      => 'https://dri.waspinventorycloud.com/public-api/',
     'user_access_token' => env('WASP_USER_ACCESS_TOKEN', null),
 ];
